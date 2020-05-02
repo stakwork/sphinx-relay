af3cc69aa92f99f1fd0e0ae1d37a1433db035d89
Evan Feenstra <evan@evanfeenstra.com>
2020-05-02 12:43:15 -0700

    single socket

--- dist/api/utils/socket.js
+++ dist/api/utils/socket.js
@@ -1,22 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const WebSocket = require("ws");
-let connections = new Map();
-let connectionCounter = 0;
+// let connections = new Map()
+// let connectionCounter = 0
+let lastConn;
 const connect = (server) => {
     server = new WebSocket.Server({ server });
     console.log('=> [socket] connected to server');
     server.on('connection', socket => {
         console.log('=> [socket] connection received');
-        var id = connectionCounter++;
-        connections.set(id, socket);
+        // var id = connectionCounter++;
+        // connections.set(id, socket)
+        lastConn = socket;
     });
 };
 exports.connect = connect;
 const send = (body) => {
-    connections.forEach((socket, index) => {
-        socket.send(body);
-    });
+    // connections.forEach((socket, index) => {
+    //   socket.send(body)
+    // })
+    lastConn.send(body);
 };
 exports.send = send;
 const sendJson = (object) => {
