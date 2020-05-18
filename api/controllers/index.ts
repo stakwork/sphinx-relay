0880bc348bf926af461fc96d9120cbfde840d298
Evan Feenstra <evan@evanfeenstra.com>
2020-05-18 11:30:40 -0700

    supervisor env

--- api/controllers/index.ts
+++ api/controllers/index.ts
@@ -33,6 +33,8 @@ async function set(app) {
 	if(testTribe && testTribe==='true') {
 		controllers.chats.testCreateTribe()
 	}
+	console.log('TEST_TRIBE',process.env.TEST_TRIBE)
+	console.log('ENV_TEST_TRIBE',process.env.ENV_TEST_TRIBE)
 
 	app.get('/chats', controllers.chats.getChats)
 	app.post('/group', controllers.chats.createGroupChat)
