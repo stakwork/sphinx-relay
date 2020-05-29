00c18cb8dd626a7bb9acc08d4904972ebd0c4462
Evan Feenstra <evan@evanfeenstra.com>
2020-05-29 11:10:28 -0700

    handle other contact QRs

--- dist/api/controllers/contacts.js
+++ dist/api/controllers/contacts.js
@@ -109,6 +109,8 @@ const createContact = (req, res) => __awaiter(void 0, void 0, void 0, function* 
         yield existing.update(updateObj);
         return res_1.success(res, jsonUtils.contactToJson(existing));
     }
+    if (attrs['public_key'].length > 66)
+        attrs['public_key'] = attrs['public_key'].subtring(0, 66);
     const createdContact = yield models_1.models.Contact.create(attrs);
     const contact = yield createdContact.update(jsonUtils.jsonToContact(attrs));
     res_1.success(res, jsonUtils.contactToJson(contact));
