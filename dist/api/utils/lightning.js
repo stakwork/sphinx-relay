c5d89e79b1341cd21f429ef788425ca0d9586925
Evan Feenstra <evan@evanfeenstra.com>
2020-06-08 08:32:40 -0700

    log

--- dist/api/utils/lightning.js
+++ dist/api/utils/lightning.js
@@ -133,6 +133,7 @@ const keysend = (opts) => {
             const call = lightning.sendPayment();
             call.on('data', function (payment) {
                 if (payment.payment_error) {
+                    console.log('=> payment.error', payment);
                     reject(payment.payment_error);
                 }
                 else {
