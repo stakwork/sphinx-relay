a4d535dd64e4475b7a29f577385d276f3b1ee1bf
Evan Feenstra <evan@evanfeenstra.com>
2020-07-15 11:58:42 -0700

    backwards compat

--- api/utils/socket.ts
+++ api/utils/socket.ts
@@ -1,16 +1,26 @@
+import * as WebSocket from 'ws'
 const socketio = require("socket.io");
 
 let io: any
+let srvr: any
 
 export function connect(server) {
   io = socketio(server);
   io.on('connection', client => {
     console.log("=> [socket.io] connected!")
   });
+
+  srvr = new WebSocket.Server({ server, clientTracking:true })
 }
 
 export const send = (body) => {
-  io.sockets.emit('message',body)
+  if(io) io.sockets.emit('message',body)
+
+  if(srvr){
+    srvr.clients.forEach(c=>{
+      if(c) c.send(body)
+    })
+  }
 }
 
 export const sendJson = (object) => {
--- dist/api/utils/socket.js
+++ dist/api/utils/socket.js
@@ -1,16 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const WebSocket = require("ws");
 const socketio = require("socket.io");
 let io;
+let srvr;
 function connect(server) {
     io = socketio(server);
     io.on('connection', client => {
         console.log("=> [socket.io] connected!");
     });
+    srvr = new WebSocket.Server({ server, clientTracking: true });
 }
 exports.connect = connect;
 exports.send = (body) => {
-    io.sockets.emit('message', body);
+    if (io)
+        io.sockets.emit('message', body);
+    if (srvr) {
+        srvr.clients.forEach(c => {
+            if (c)
+                c.send(body);
+        });
+    }
 };
 exports.sendJson = (object) => {
     exports.send(JSON.stringify(object));
