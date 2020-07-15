b254fcfb4f6aefe7d23faea79a6d53f5040dcd78
Evan Feenstra <evan@evanfeenstra.com>
2020-07-15 11:02:02 -0700

    default socketio config

--- api/utils/socket.ts
+++ api/utils/socket.ts
@@ -3,22 +3,15 @@ const socketio = require("socket.io");
 let io: any
 
 function connect(server) {
-  io = socketio(server, {
-    // path: '/socket',
-    serveClient: false,
-    // below are engine.IO options
-    pingInterval: 10000,
-    pingTimeout: 5000,
-    cookie: false
-  });
+  io = socketio(server);
 
   io.on('connection', client => {
     console.log("=> [socket.io] connected!")
     client.on('event', data => { /* … */ });
     client.on('disconnect', () => { /* … */ });
 
-    io.sockets.send('{"try":"try"}')
-    client.send('{"try":"try"}')
+    // io.sockets.send('{"try":"try"}')
+    // client.send('{"try":"try"}')
   });
 }
 
--- dist/api/utils/socket.js
+++ dist/api/utils/socket.js
@@ -3,20 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const socketio = require("socket.io");
 let io;
 function connect(server) {
-    io = socketio(server, {
-        // path: '/socket',
-        serveClient: false,
-        // below are engine.IO options
-        pingInterval: 10000,
-        pingTimeout: 5000,
-        cookie: false
-    });
+    io = socketio(server);
     io.on('connection', client => {
         console.log("=> [socket.io] connected!");
         client.on('event', data => { });
         client.on('disconnect', () => { });
-        io.sockets.send('{"try":"try"}');
-        client.send('{"try":"try"}');
+        // io.sockets.send('{"try":"try"}')
+        // client.send('{"try":"try"}')
     });
 }
 exports.connect = connect;
