301b3811c36a62656db69bf283520d0ddd998ea2
Evan Feenstra <evan@evanfeenstra.com>
2020-09-09 11:43:54 -0700

    btc price bot test

---
+++ api/bots/btc.ts
@@ -0,0 +1,72 @@
+import * as Sphinx from 'sphinx-bot'
+import { finalAction } from '../controllers/actions'
+import * as fetch from 'node-fetch'
+const msg_types = Sphinx.MSG_TYPE
+
+let initted = false
+
+/*
+curl -H "X-CMC_PRO_API_KEY: 2fddf900-9114-471c-849d-cad3e3923e5b" -H "Accept: application/json" -d "symbol=BTC&convert=USD" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest
+*/
+
+const token = '2fddf900-9114-471c-849d-cad3e3923e5b'
+const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+export function init() {
+  if(initted) return
+  initted = true
+
+  const client = new Sphinx.Client()
+  client.login('_', finalAction)
+
+  client.on(msg_types.MESSAGE, async (message: Sphinx.Message) => {
+    const arr = message.content.split(' ')
+    if (arr.length < 2) return
+    if (arr[0]!=='/btc') return
+    const cmd = arr[1]
+    
+    switch (cmd) {
+
+      case 'price':
+        if (arr.length < 3) return
+        console.log("price", arr[2])
+        try {
+            const r = await fetch(url+'?symbol=BTC&convert=USD',{
+                headers:{'X-CMC_PRO_API_KEY': token, 'Accept': 'application/json'}
+            })
+            if (!r.ok) return
+            const j = await r.json()
+            const price = j.data.BTC.quote.USD.price.toFixed(2)
+            const percentChange24 = j.data.BTC.quote.USD.percent_change_24h.toFixed(2)
+            const embed = new Sphinx.MessageEmbed()
+                .setAuthor('BitcoinBot')
+                .setTitle('Bitcoin Price:')
+                .addFields([
+                { name: 'Price:', value: price },
+                { name: '24 Hour Change', value: percentChange24+'%' }
+                ])
+                .setThumbnail(botSVG)
+            message.channel.send({ embed })
+        } catch(e){
+            console.log('BTC bot error',e)
+        }
+        return
+        
+      default:
+        const embed = new Sphinx.MessageEmbed()
+          .setAuthor('BitcoinBot')
+          .setTitle('BitcoinBot Commands:')
+          .addFields([
+            { name: 'Print BTC price', value: '/btc price' },
+            { name: 'Help', value: '/btc help' }
+          ])
+          .setThumbnail(botSVG)
+        message.channel.send({ embed })
+        return
+    }
+  })
+}
+
+const botSVG = `<svg viewBox="64 64 896 896" height="12" width="12" fill="white">
+  <path d="M300 328a60 60 0 10120 0 60 60 0 10-120 0zM852 64H172c-17.7 0-32 14.3-32 32v660c0 17.7 14.3 32 32 32h680c17.7 0 32-14.3 32-32V96c0-17.7-14.3-32-32-32zm-32 660H204V128h616v596zM604 328a60 60 0 10120 0 60 60 0 10-120 0zm250.2 556H169.8c-16.5 0-29.8 14.3-29.8 32v36c0 4.4 3.3 8 7.4 8h729.1c4.1 0 7.4-3.6 7.4-8v-36c.1-17.7-13.2-32-29.7-32zM664 508H360c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h304c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z" />
+</svg>`
--- dist/api/bots/btc.js
+++ dist/api/bots/btc.js
@@ -11,11 +11,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sphinx = require("sphinx-bot");
 const actions_1 = require("../controllers/actions");
-const models_1 = require("../models");
-const path = require("path");
+const fetch = require("node-fetch");
 const msg_types = Sphinx.MSG_TYPE;
-const constants = require(path.join(__dirname, '../../config/constants.json'));
 let initted = false;
+/*
+curl -H "X-CMC_PRO_API_KEY: 2fddf900-9114-471c-849d-cad3e3923e5b" -H "Accept: application/json" -d "symbol=BTC&convert=USD" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest
+*/
+const token = '2fddf900-9114-471c-849d-cad3e3923e5b';
+const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
 function init() {
     if (initted)
         return;
@@ -24,55 +27,46 @@ function init() {
     client.login('_', actions_1.finalAction);
     client.on(msg_types.MESSAGE, (message) => __awaiter(this, void 0, void 0, function* () {
         const arr = message.content.split(' ');
-        console.log('message.type', message.type);
-        const isGroupJoin = message.type === constants.message_types.group_join;
-        if (arr.length < 2 && !isGroupJoin)
+        if (arr.length < 2)
             return;
-        if (arr[0] !== '/welcome' && !isGroupJoin)
+        if (arr[0] !== '/btc')
             return;
         const cmd = arr[1];
-        if (isGroupJoin) {
-            const chat = yield models_1.models.Chat.findOne({ where: { uuid: message.channel.id } });
-            if (!chat)
-                return;
-            const chatBot = yield models_1.models.ChatBot.findOne({ where: {
-                    chatId: chat.id, botPrefix: '/welcome', botType: constants.bot_types.builtin
-                } });
-            if (!(chatBot && chatBot.meta))
-                return;
-            const resEmbed = new Sphinx.MessageEmbed()
-                .setAuthor('WelcomeBot')
-                .setDescription(chatBot.meta);
-            message.channel.send({ embed: resEmbed });
-            return;
-        }
         switch (cmd) {
-            case 'setmessage':
+            case 'price':
                 if (arr.length < 3)
                     return;
-                console.log("setmsg", arr[2]);
-                const chat = yield models_1.models.Chat.findOne({ where: { uuid: message.channel.id } });
-                if (!chat)
-                    return;
-                const chatBot = yield models_1.models.ChatBot.findOne({ where: {
-                        chatId: chat.id, botPrefix: '/welcome', botType: constants.bot_types.builtin
-                    } });
-                if (!chatBot)
-                    return;
-                const meta = arr.slice(2, arr.length).join(' ');
-                yield chatBot.update({ meta });
-                const resEmbed = new Sphinx.MessageEmbed()
-                    .setAuthor('WelcomeBot')
-                    .setDescription('Your welcome message has been updated');
-                message.channel.send({ embed: resEmbed });
+                console.log("price", arr[2]);
+                try {
+                    const r = yield fetch(url + '?symbol=BTC&convert=USD', {
+                        headers: { 'X-CMC_PRO_API_KEY': token, 'Accept': 'application/json' }
+                    });
+                    if (!r.ok)
+                        return;
+                    const j = yield r.json();
+                    const price = j.data.BTC.quote.USD.price.toFixed(2);
+                    const percentChange24 = j.data.BTC.quote.USD.percent_change_24h.toFixed(2);
+                    const embed = new Sphinx.MessageEmbed()
+                        .setAuthor('BitcoinBot')
+                        .setTitle('Bitcoin Price:')
+                        .addFields([
+                        { name: 'Price:', value: price },
+                        { name: '24 Hour Change', value: percentChange24 + '%' }
+                    ])
+                        .setThumbnail(botSVG);
+                    message.channel.send({ embed });
+                }
+                catch (e) {
+                    console.log('BTC bot error', e);
+                }
                 return;
             default:
                 const embed = new Sphinx.MessageEmbed()
-                    .setAuthor('WelcomeBot')
-                    .setTitle('Bot Commands:')
+                    .setAuthor('BitcoinBot')
+                    .setTitle('BitcoinBot Commands:')
                     .addFields([
-                    { name: 'Set welcome message', value: '/welcome setmessage {MESSAGE}' },
-                    { name: 'Help', value: '/welcome help' }
+                    { name: 'Print BTC price', value: '/btc price' },
+                    { name: 'Help', value: '/btc help' }
                 ])
                     .setThumbnail(botSVG);
                 message.channel.send({ embed });
@@ -84,4 +78,4 @@ exports.init = init;
 const botSVG = `<svg viewBox="64 64 896 896" height="12" width="12" fill="white">
   <path d="M300 328a60 60 0 10120 0 60 60 0 10-120 0zM852 64H172c-17.7 0-32 14.3-32 32v660c0 17.7 14.3 32 32 32h680c17.7 0 32-14.3 32-32V96c0-17.7-14.3-32-32-32zm-32 660H204V128h616v596zM604 328a60 60 0 10120 0 60 60 0 10-120 0zm250.2 556H169.8c-16.5 0-29.8 14.3-29.8 32v36c0 4.4 3.3 8 7.4 8h729.1c4.1 0 7.4-3.6 7.4-8v-36c.1-17.7-13.2-32-29.7-32zM664 508H360c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h304c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z" />
 </svg>`;
-//# sourceMappingURL=welcome.js.map
+//# sourceMappingURL=btc.js.map
