4d583cf03193abe484756803872ce4d5b9e4f505
Evan Feenstra <evan@evanfeenstra.com>
2020-09-09 13:19:15 -0700

    btc bot format

--- dist/api/bots/btc.js
+++ dist/api/bots/btc.js
@@ -42,16 +42,16 @@ function init() {
                     if (!r.ok)
                         return;
                     const j = yield r.json();
-                    const price = j.data.BTC.quote.USD.price.toFixed(2);
+                    const price = '$' + j.data.BTC.quote.USD.price.toFixed(2);
                     const percentChange24 = j.data.BTC.quote.USD.percent_change_24h;
                     const percentChange24String = percentChange24.toFixed(2) + '%';
-                    const changeColor = percentChange24 > 0 ? '#078864' : '#7a2d2f';
+                    const changeColor = percentChange24 > 0 ? '#00c991' : '#7a2d2f';
                     const embed = new Sphinx.MessageEmbed()
                         .setAuthor('BitcoinBot')
                         .setTitle('Bitcoin Price:')
                         .addFields([
                         { name: 'Price:', value: price, inline: true },
-                        { name: '24 Hour Change', value: percentChange24String, inline: true, color: changeColor }
+                        { name: '24 Hour Change:', value: percentChange24String, inline: true, color: changeColor }
                     ])
                         .setThumbnail(botSVG);
                     message.channel.send({ embed });
