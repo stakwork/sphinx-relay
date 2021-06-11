7d1bd49a9c464354efa94fcc6fa34ff11e52a5e1
Evanfeenstra <evanfeenstra@gmail.com>
2021-06-11 15:10:13 -0700

    more greenlight interfaces

--- dist/src/grpc/interfaces.js
+++ dist/src/grpc/interfaces.js
@@ -1,20 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getInfoResponse = void 0;
+exports.addInvoiceRequest = exports.getInfoResponse = void 0;
+const config_1 = require("../utils/config");
+const config = config_1.loadConfig();
+const IS_LND = config.lightning_provider === 'LND';
+const IS_GREENLIGHT = config.lightning_provider === 'GREENLIGHT';
 function getInfoResponse(res) {
-    if ('identity_pubkey' in res) { // LND
+    if (IS_LND) { // LND
         return res;
     }
-    if ('node_id' in res) { // greenlight
+    if (IS_GREENLIGHT) { // greenlight
+        const r = res;
         return {
-            identity_pubkey: Buffer.from(res.node_id).toString('hex'),
-            version: res.version,
-            alias: res.alias,
-            color: res.color,
-            num_peers: res.num_peers,
+            identity_pubkey: Buffer.from(r.node_id).toString('hex'),
+            version: r.version,
+            alias: r.alias,
+            color: r.color,
+            num_peers: r.num_peers,
         };
     }
     return {};
 }
 exports.getInfoResponse = getInfoResponse;
+function addInvoiceRequest(req) {
+    if (IS_LND)
+        return req;
+    if (IS_GREENLIGHT) {
+        return {
+            satoshi: req.value,
+            label: req.memo,
+            description: req.memo,
+        };
+    }
+    return {};
+}
+exports.addInvoiceRequest = addInvoiceRequest;
+/*
+GREENLIGHT NEEDS
+
+route hints in Create Invoice
+
+custom TLV fields Pay (for adding data, and doing keysend payments)
+
+
+*/ 
 //# sourceMappingURL=interfaces.js.map
--- src/grpc/interfaces.ts
+++ src/grpc/interfaces.ts
@@ -1,4 +1,9 @@
+import { loadConfig } from '../utils/config'
 
+const config = loadConfig()
+
+const IS_LND = config.lightning_provider==='LND'
+const IS_GREENLIGHT = config.lightning_provider==='GREENLIGHT'
 
 interface Feature {
   name: string;
@@ -28,19 +33,79 @@ interface GetInfoResponse {
   chains: Chain[];
   features: { [k: number]: Feature };
 }
-export function getInfoResponse(res): GetInfoResponse {
-  if ('identity_pubkey' in res) { // LND
-    return res;
+interface GreenlightAddress {
+  type: number
+	addr: string
+	port: number
+}
+interface GreenlightGetInfoResponse {
+    node_id: ArrayBuffer
+    alias: string
+    color: string
+    num_peers: number
+    addresses: GreenlightAddress[]
+    version: string
+    blockheight: number
+    network: string
+}
+export function getInfoResponse(res:GetInfoResponse|GreenlightGetInfoResponse): GetInfoResponse {
+  if (IS_LND) { // LND
+    return res as GetInfoResponse;
   }
-  if ('node_id' in res) { // greenlight
+  if (IS_GREENLIGHT) { // greenlight
+    const r = res as GreenlightGetInfoResponse
     return <GetInfoResponse>{
-      identity_pubkey: Buffer.from(res.node_id).toString('hex'),
-      version: res.version,
-      alias: res.alias,
-      color: res.color,
-      num_peers: res.num_peers,
+      identity_pubkey: Buffer.from(r.node_id).toString('hex'),
+      version: r.version,
+      alias: r.alias,
+      color: r.color,
+      num_peers: r.num_peers,
     };
   }
   return <GetInfoResponse>{}
 }
 
+interface HopHint {
+  node_id: string
+  chan_id: string
+  fee_base_msat: number
+  fee_proportional_millionths: number
+  cltv_expiry_delta: number
+}
+interface RouteHint {
+  hop_hints: HopHint[]
+}
+interface AddInvoiceRequest {
+  memo: string
+  value: number
+  route_hints: RouteHint[]
+  expiry: number
+}
+interface GreenlightAddInvoiceRequest {
+  millisatoshi?: number
+  satoshi?: number
+  bitcoin?: number
+  label: string
+  description: string
+}
+export function addInvoiceRequest(req:AddInvoiceRequest): AddInvoiceRequest|GreenlightAddInvoiceRequest {
+  if(IS_LND) return req
+  if(IS_GREENLIGHT) {
+    return <GreenlightAddInvoiceRequest>{
+      satoshi: req.value,
+      label: req.memo,
+      description: req.memo,
+    }
+  }
+  return <AddInvoiceRequest>{}
+}
+
+/*
+GREENLIGHT NEEDS
+
+route hints in Create Invoice
+
+custom TLV fields Pay (for adding data, and doing keysend payments)
+
+
+*/
