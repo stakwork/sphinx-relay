24f2e9fed95f7455fec9edfc55c93126fff13d5e
Buck Perley <bucko.perley@gmail.com>
2021-09-12 14:18:35 -0500

    remove dist from repo before git ignoring

--- dist/src/utils/lock.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AsyncLock = require("async-lock");
-const lock = new AsyncLock();
-exports.default = lock;
-//# sourceMappingURL=lock.js.map
--- dist/src/utils/password.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const crypto = require("crypto");
-const password = crypto.randomBytes(16).toString('hex');
-exports.default = password;
-//# sourceMappingURL=password.js.map
