3eaef4939a2416247007baa01d6a4921d7214900
Kevin Pallikunnel <oapallikunnel@gmail.com>
2021-10-27 09:12:16 -0500

    adding tribe images test

---
+++ src/tests/controllers/tribeImages.test.ts
@@ -0,0 +1,52 @@
+var test = require('ava')
+import { iterate } from '../utils/helpers'
+import nodes from '../nodes'
+import { createTribe, joinTribe } from '../utils/save'
+import { deleteTribe, leaveTribe } from '../utils/del'
+import { sendImage } from '../utils/msg'
+import { greenSquare, pinkSquare } from '../configs/b64-images'
+
+/*
+npx ava test-05-tribeImages.js --verbose --serial --timeout=2m
+*/
+
+test('test-05-tribeImages: create tribe, join tribe, send images, leave tribe, delete tribe', async (t) => {
+  await iterate(nodes, async (node1, node2) => {
+    await tribeImages(t, node1, node2)
+  })
+})
+
+async function tribeImages(t, node1, node2) {
+  //TWO NODES SEND IMAGES WITHIN A TRIBE ===>
+
+  console.log(`${node1.alias} and ${node2.alias}`)
+
+  //NODE1 CREATES A TRIBE
+  let tribe = await createTribe(t, node1)
+  t.truthy(tribe, 'tribe should have been created by node1')
+
+  //NODE2 JOINS TRIBE CREATED BY NODE1
+  if (node1.routeHint) tribe.owner_route_hint = node1.routeHint
+  let join = await joinTribe(t, node2, tribe)
+  t.true(join, 'node2 should join tribe')
+
+  //NODE1 SEND IMAGE TO NODE2
+  const image = greenSquare
+  const imageSent = await sendImage(t, node1, node2, image, tribe)
+  t.true(imageSent, 'message should have been sent')
+
+  //NODE2 SENDS AN IMAGE TO NODE1
+  const image2 = pinkSquare
+  const imageSent2 = await sendImage(t, node2, node1, image2, tribe)
+  t.true(imageSent2, 'message should have been sent')
+
+  //NODE2 LEAVES TRIBE
+  let left2 = await leaveTribe(t, node2, tribe)
+  t.true(left2, 'node2 should leave tribe')
+
+  //NODE1 DELETES TRIBE
+  let delTribe2 = await deleteTribe(t, node1, tribe)
+  t.true(delTribe2, 'node1 should delete tribe')
+}
+
+module.exports = tribeImages
--- src/tests/utils/msg/index.ts
+++ src/tests/utils/msg/index.ts
@@ -1,3 +1,4 @@
 export { sendMessageAndCheckDecryption } from './sendMessageAndCheckDecryption'
 export { sendTribeMessageAndCheckDecryption } from './sendTribeMessageAndCheckDecryption'
 export { sendBoost } from './sendBoost'
+export { sendImage } from './sendImage'
