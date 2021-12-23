e8c026cc1e5b5f2147d1180a85a94fb42690d49a
Kevin Pallikunnel <oapallikunnel@gmail.com>
2021-12-23 09:29:04 -0600

    checking what the error is

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -32,7 +32,7 @@ async function setVersion() {
   try {
     await sequelize.query(`PRAGMA user_version = ${USER_VERSION}`)
   } catch (e) {
-    console.log('=> [db] setVersion failed')
+    console.log('=> [db] setVersion failed', e)
   }
 }
 
