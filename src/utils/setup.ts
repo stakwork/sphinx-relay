08751351ea1ca62f5ca0db8122ef331c953fc96d
Kevin Pallikunnel <oapallikunnel@gmail.com>
2021-12-28 08:46:05 -0600

    removed some logging

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -32,7 +32,7 @@ async function setVersion() {
   try {
     await sequelize.query(`PRAGMA user_version = ${USER_VERSION}`)
   } catch (e) {
-    console.log('=> [db] setVersion has  weee failed', e)
+    console.log('=> [db] setVersion has failed')
   }
 }
 
