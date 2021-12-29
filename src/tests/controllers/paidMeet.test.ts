bd2584b0f3c7137befcafd7b5a9ecc8f702dc674
Kevin Pallikunnel <oapallikunnel@gmail.com>
2021-12-29 09:18:07 -0600

    removing from the diff

--- src/tests/controllers/paidMeet.test.ts
+++ src/tests/controllers/paidMeet.test.ts
@@ -38,10 +38,6 @@ async function paidMeet(t, node1, node2) {
   const self = await getSelf(t, node1)
   t.true(self.price_to_meet === 13, 'node1 should have updated price_to_meet')
 
-  //DELETE ALL CONTACTS
-  const clearToStart = await clearAllContacts(t)
-  t.truthy(clearToStart, 'all contacts should be cleared')
-
   //NODE2 ADDS NODE1 AS A CONTACT
   let added = await addContact(t, node2, node1)
   t.true(added, 'node2 should add node1 as contact')
