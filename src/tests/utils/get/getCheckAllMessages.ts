65a5e97ad056ca6168dc853bdc51ad3aefb17b47
Kevin Pallikunnel <oapallikunnel@gmail.com>
2022-01-26 17:04:12 -0600

    updating the test for the new flag that was added without a test

--- src/tests/utils/get/getCheckAllMessages.ts
+++ src/tests/utils/get/getCheckAllMessages.ts
@@ -7,11 +7,12 @@ export function getCheckAllMessages(
   _t: Assertions,
   node: NodeConfig,
   limit: number,
-  offset: number
+  offset: number,
+  order: string = 'asc'
 ): Promise<{ new_messages: Array<Message>; new_messages_total: number }> {
   return new Promise((resolve, reject) => {
     setTimeout(async () => {
-      timeout(0, node, limit, offset, resolve, reject)
+      timeout(0, node, limit, offset, order, resolve, reject)
     }, 1000)
   })
 }
@@ -21,11 +22,12 @@ async function timeout(
   node: NodeConfig,
   limit: number,
   offset: number,
+  order: string,
   resolve,
   reject
 ) {
   const msgRes = await http.get(
-    `${node.external_ip}/allmessages?limit=${limit}&offset=${offset}`,
+    `${node.external_ip}/allmessages?limit=${limit}&offset=${offset}&order=${order}`,
     makeArgs(node)
   )
   if (msgRes.response.new_messages && msgRes.response.new_messages.length) {
@@ -36,6 +38,6 @@ async function timeout(
     return reject('failed to getCheckMsgs')
   }
   setTimeout(async () => {
-    timeout(i + 1, node, limit, offset, resolve, reject)
+    timeout(i + 1, node, limit, offset, order, resolve, reject)
   }, 1000)
 }
