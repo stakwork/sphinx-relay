64febe64c87d711b9ef35f380de97ca7edacb796
Kevin Pallikunnel <oapallikunnel@gmail.com>
2022-03-08 13:32:42 -0600

    adding delete message socketio test

--- src/tests/controllers/socketIO.test.ts
+++ src/tests/controllers/socketIO.test.ts
@@ -4,7 +4,7 @@ import * as socketio from 'socket.io-client'
 import * as socketiolegacy from 'socket.io-client-legacy'
 import nodes from '../nodes'
 
-import { randomText } from '../utils/helpers'
+import { randomText, sleep } from '../utils/helpers'
 
 //import { tribe3Msgs } from './tribe3Messages.test'
 import {
@@ -15,9 +15,10 @@ import {
 } from '../utils/msg'
 
 import { addContact, joinTribe, createTribe } from '../utils/save'
+import { deleteMessage } from '../utils/del'
 
 /*
-  npx ava src/tests/controllers/chatInvoice.test.ts --verbose --serial --timeout=2m
+  npx ava src/tests/controllers/socketIO.test.ts --verbose --serial --timeout=2m
 */
 
 interface Context {}
@@ -28,13 +29,10 @@ let handlers: { [k: string]: DataHandler } = {}
 let io: any = null
 var responseArray: any[] = []
 
-test.serial(
-  'test-09-chatInvoice: add contact, send invoices, pay invoices, delete contact',
-  async (t: ExecutionContext<Context>) => {
-    await testSocketIO(t, false)
-    await testSocketIO(t, true)
-  }
-)
+test.serial('test-09-socketIO', async (t: ExecutionContext<Context>) => {
+  await testSocketIO(t, false)
+  //await testSocketIO(t, true)
+})
 
 async function testSocketIO(t: ExecutionContext<Context>, legacy: boolean) {
   connectWebSocket(
@@ -45,22 +43,22 @@ async function testSocketIO(t: ExecutionContext<Context>, legacy: boolean) {
     () => {}
   )
   io.connect()
-  //    tribe3Msgs(t, nodes[0], nodes[1], nodes[2])
   await addContact(t, nodes[0], nodes[1])
   console.log('\nafter add contact')
   responseArray.forEach((item) => console.log(item.type))
+  sleep(10000)
 
   //*******
   //Receive payment
   //payment.ts
   const amount = 101
   const paymentText = 'this eleven payment'
-  await sendPayment(t, nodes[0], nodes[1], amount, paymentText)
+
+  const payment = await sendPayment(t, nodes[0], nodes[1], amount, paymentText)
+  await sleep(10000)
   console.log('\nafter send payment')
   responseArray.forEach((item) => console.log(item.type))
-  const payment = await sendPayment(t, nodes[0], nodes[1], amount, paymentText)
-  console.log('\nafter second send payment')
-  responseArray.forEach((item) => console.log(item.type))
+
   t.true(payment, 'payment should be sent')
 
   t.true(
@@ -70,11 +68,11 @@ async function testSocketIO(t: ExecutionContext<Context>, legacy: boolean) {
   )
   t.true(
     responseArray[responseArray.length - 1].response.amount == amount,
-    'payment should be sent'
+    'payment should be sent with correct amount'
   )
   t.true(
     responseArray[responseArray.length - 1].type == 'direct_payment',
-    'payment should be sent'
+    'payment should have the correct type'
   )
 
   //*******
@@ -82,7 +80,12 @@ async function testSocketIO(t: ExecutionContext<Context>, legacy: boolean) {
   //messages.ts
   //********
   const messageText = randomText()
-  await sendMessageAndCheckDecryption(t, nodes[0], nodes[1], messageText)
+  const sentMessage = await sendMessageAndCheckDecryption(
+    t,
+    nodes[0],
+    nodes[1],
+    messageText
+  )
   console.log('\nafter send direct message')
   responseArray.forEach((item) => console.log(item.type))
   t.true(
@@ -129,6 +132,13 @@ async function testSocketIO(t: ExecutionContext<Context>, legacy: boolean) {
   t.true(
     responseArray[responseArray.length - 1].type == 'boost',
     'we should get back something when we recieve a message'
+  )
+
+  await deleteMessage(t, nodes[0], sentMessage.id)
+  await sleep(10000)
+  t.true(
+    responseArray[responseArray.length - 1].type == 'delete',
+    'we should get back a delete type'
   )
   //console.log('This is', responseArray)
   //******
