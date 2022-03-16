702eb7491d56def6191156187c3e3d23ece1611f
evanfeenstra <evanfeenstra@gmail.com>
2022-03-16 13:45:58 -0700

    proper network msg interface

--- src/network/index.ts
+++ src/network/index.ts
@@ -8,7 +8,7 @@ import {
   typesToSkipIfSkipBroadcastJoins,
   receiveMqttMessage,
 } from './receive'
-import { Msg } from './interfaces'
+import { Msg, BotMsg, Payload } from './interfaces'
 
 /*
 Abstracts between lightning network and MQTT depending on Chat type and sender
@@ -26,4 +26,6 @@ export {
   typesToSkipIfSkipBroadcastJoins,
   receiveMqttMessage,
   Msg,
+  BotMsg,
+  Payload,
 }
