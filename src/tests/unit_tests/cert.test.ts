bb8d84fe19f6f4255fbc4509798854f051358996
Kevin Pallikunnel <oapallikunnel@gmail.com>
2022-08-19 22:07:39 -0600

    added some more coverage and updated workflow so the label is correct

--- src/tests/unit_tests/cert.test.ts
+++ src/tests/unit_tests/cert.test.ts
@@ -1,18 +1,24 @@
-import { toSnake } from '../../utils/case'
+import { toSnake, toCamel } from '../../utils/case'
 
-describe('Calculator Tests', () => {
-  test('Addition of 2 numbers', async () => {
-    const notSnakeObj = {
-      superCamelCase: 20,
-      nextValueHere: 'words',
-    }
+describe('tests for src/utils/case', () => {
+  const notSnakeObj = {
+    superCamelCase: 20,
+    nextValueHere: 'words',
+  }
+  const correct_snaked_string = {
+    super_camel_case: 20,
+    next_value_here: 'words',
+  }
 
+  test('toSnake', async () => {
     const snaked_string = toSnake(notSnakeObj)
-    const correct_snaked_string = {
-      super_camel_case: 20,
-      next_value_here: 'words',
-    }
 
     expect(snaked_string).toStrictEqual(correct_snaked_string)
+  })
+
+  test('toCamel', async () => {
+    const camel_string = toCamel(correct_snaked_string)
+
+    expect(camel_string).toStrictEqual(notSnakeObj)
   })
 })
