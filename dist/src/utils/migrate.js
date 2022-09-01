a27e6045082882ce91f43b17ecf2baf56cdc3079
Evan Feenstra <evanfeenstra@gmail.com>
2022-09-01 10:21:33 -0700

    make sure meta column on bot table

--- dist/src/utils/migrate.js
+++ dist/src/utils/migrate.js
@@ -98,6 +98,7 @@ function migrate() {
         catch (e) {
             //Do nothing here
         }
+        addTableColumn('sphinx_bots', 'meta');
         addTableColumn('sphinx_bots', 'webhook');
         addTableColumn('sphinx_bots', 'uuid');
         addTableColumn('sphinx_bots', 'price_per_use', 'INT');
