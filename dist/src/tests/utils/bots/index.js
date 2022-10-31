17825b7ff9f5ef826ab4ace49fc92f1254d26789
Omoniyi Ilesanmi <omoniyi.ilesanmi@prunny.com>
2022-10-31 22:03:08 +0100

    remove noise files

--- dist/src/tests/utils/bots/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.botDecrypt = void 0;
-var botDecrypt_1 = require("./botDecrypt");
-Object.defineProperty(exports, "botDecrypt", { enumerable: true, get: function () { return botDecrypt_1.botDecrypt; } });
-//# sourceMappingURL=index.js.map
--- dist/src/tests/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.saveHelpers = exports.getHelpers = exports.helpers = void 0;
-exports.helpers = require("./helpers");
-exports.getHelpers = require("./get");
-exports.saveHelpers = require("./save");
-//# sourceMappingURL=index.js.map
