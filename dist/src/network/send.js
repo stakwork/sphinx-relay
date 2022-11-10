20a3fc74681244b1cb209d85d733360be7b3b14d
Oluwatobi Bamidele <teebams49@gmail.com>
2022-11-10 16:59:10 +0100

    fix: person uuid

--- dist/src/network/send.js
+++ dist/src/network/send.js
@@ -20,7 +20,7 @@ const intercept = require("./intercept");
 const constants_1 = require("../constants");
 const logger_1 = require("../utils/logger");
 const config_1 = require("../utils/config");
-const people = require("../utils/people");
+// import * as people from '../utils/people'
 const config = (0, config_1.loadConfig)();
 function sendMessage({ type, chat, message, sender, amount, success, failure, skipPubKey, isForwarded, forwardedFromContactId, realSatsContactId, }) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -286,12 +286,15 @@ function newmsg(type, chat, sender, message, isForwarded, includeStatus) {
             person: `${config.people_host}/${sender.personUuid}`,
         })), { alias: includeAlias ? aliasToInclude : '', role: sender.role || constants_1.default.chat_roles.reader }), (includePhotoUrl && { photo_url: photoUrlToInclude })),
     };
-    const personId = people.getPersonId();
-    if (personId) {
-        result.sender.person = config.people_host + '/' + personId;
-        logger_1.sphinxLogger.info(`[+] person host full URL  ${result.sender.person}`, logger_1.logging.Network);
-        return result;
-    }
+    // const personId = people.getPersonId()
+    // if (personId) {
+    //   result.sender.person = config.people_host + '/' + personId
+    //   sphinxLogger.info(
+    //     `[+] person host full URL  ${result.sender.person}`,
+    //     logging.Network
+    //   )
+    //   return result
+    // }
     return result;
 }
 exports.newmsg = newmsg;
