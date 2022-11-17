46349fbc0787a5bed062ec1b831b92b35464dcc7
Kevin Pallikunnel <oapallikunnel@gmail.com>
2022-11-17 11:04:42 -0600

    test: getChats to see tribe info
    right now the second node is not seeing the pinned messages so I want to
    add a test to see the new pinned messages
    
    just logging from the getChats call and I'm seeing `PIN` in node1 but
    not node2

--- src/tests/controllers/pinnedMsg.test.ts
+++ src/tests/controllers/pinnedMsg.test.ts
@@ -1,7 +1,8 @@
 import test, { ExecutionContext } from 'ava'
 import nodes from '../nodes'
 import { deleteTribe } from '../utils/del'
-import { createTribe, pinMsgToTribe } from '../utils/save'
+import { getChats } from '../utils/get'
+import { createTribe, pinMsgToTribe, joinTribe } from '../utils/save'
 import { NodeConfig } from '../types'
 
 /*
@@ -12,15 +13,25 @@ interface Context {}
 
 test.serial('tribe pinned msg', async (t: ExecutionContext<Context>) => {
   t.true(Array.isArray(nodes))
-  await tribeTest(t, nodes[0])
+  await tribeTest(t, nodes[0], nodes[1])
 })
 
-async function tribeTest(t: ExecutionContext<Context>, node1: NodeConfig) {
+async function tribeTest(
+  t: ExecutionContext<Context>,
+  node1: NodeConfig,
+  node2: NodeConfig
+) {
   let tribe = await createTribe(t, node1)
   t.truthy(tribe, 'tribe should have been created by node1')
 
   const pin = await pinMsgToTribe(t, node1, tribe.id, 'PIN')
   t.true(pin === 'PIN', 'pin should equal')
+
+  let join = await joinTribe(t, node2, tribe)
+  t.true(join, 'node2 should join tribe')
+  let tribes2 = await getChats(t, node1)
+  //let tribes = await getChats(t, node2)
+  console.log(tribes2)
 
   let delTribe = await deleteTribe(t, node1, tribe)
   t.true(delTribe, 'node1 should delete tribe')
