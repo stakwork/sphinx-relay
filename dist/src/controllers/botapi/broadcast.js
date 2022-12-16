7ed9bcc3a58fc741c848ecba8a82c0ff417fc65a
Kevin Pallikunnel <oapallikunnel@gmail.com>
2022-12-16 12:40:59 -0600

    fix: unseen chat

--- dist/src/controllers/botapi/broadcast.js
+++ dist/src/controllers/botapi/broadcast.js
@@ -49,12 +49,16 @@ function broadcast(a) {
             updatedAt: date,
             senderAlias: alias,
             tenant,
-            seen: false,
         };
         if (parent_id)
             msg.parentId = parent_id;
         if (bot_pic)
             msg.senderPic = bot_pic;
+        let unseenChat = (yield models_1.models.Chat.findOne({
+            where: { id: chat.id, tenant },
+        }));
+        if (unseenChat)
+            unseenChat.update({ seen: false });
         const message = (yield models_1.models.Message.create(msg));
         socket.sendJson({
             type: 'message',
