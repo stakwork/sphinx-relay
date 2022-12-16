e07bf5830e9a443af1af4cb0226e704f6a6a4134
Oluwatobi Bamidele <teebams49@gmail.com>
2022-12-16 22:35:49 +0100

    feat: get action history

---
+++ src/tests/controllers/actionHistory.test.ts
@@ -0,0 +1,21 @@
+import test from 'ava'
+import nodes from '../nodes'
+import { saveActionHistory } from '../utils/save'
+
+/*
+npx ava src/tests/controllers/actionHistory.test.ts --verbose --serial --timeout=2m
+*/
+
+test('test boostPayment: create tribe, join tribe, send messages, boost messages, leave tribe, delete tribe', async (t) => {
+  await actionHistory(t, 0)
+})
+
+export async function actionHistory(t, index) {
+  let node = nodes[index]
+
+  console.log(`Testing Action History for ${node.alias}`)
+
+  const searchTerm = 'search for utxo'
+  const saveAction = await saveActionHistory(t, searchTerm, node)
+  t.true(saveAction, 'Action needs to be saved on the DB')
+}
