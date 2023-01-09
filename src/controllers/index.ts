fc975725d178d08a807b7071d6ae73d03538ccaf
Evan Feenstra <evanfeenstra@gmail.com>
2023-01-09 14:37:13 -0800

    has admin route

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -154,6 +154,7 @@ export async function set(app) {
 
   app.get('/add_user', admin.addProxyUser)
   app.get('/list_users', admin.listUsers)
+  app.get('/has_admin', admin.hasAdmin)
 
   app.get('/version', async function (req: Req, res) {
     res.send({ version: gitinfo.tag })
