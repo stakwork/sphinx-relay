b539764d490f7669a4e0b4e5dd18ab02a20b1452
Evan Feenstra <evanfeenstra@gmail.com>
2023-01-12 13:03:50 -0800

    fix stakwork_api_key migrate

--- src/utils/migrate.ts
+++ src/utils/migrate.ts
@@ -280,7 +280,7 @@ export default async function migrate(): Promise<void> {
   addTableColumn('sphinx_chats', 'call_recording', 'INT')
   addTableColumn('sphinx_chats', 'meme_server_location', 'TEXT')
   addTableColumn('sphinx_chats', 'jitsi_server', 'TEXT')
-  addTableColumn('sphinx_chars', 'stakwork_api_key', 'TEXT')
+  addTableColumn('sphinx_chats', 'stakwork_api_key', 'TEXT')
   addTableColumn('sphinx_chats', 'stakwork_webhook', 'TEXT')
 }
 
