09087e71b6b652935db3d1f726089f60948f4097
Oluwatobi Bamidele <teebams49@gmail.com>
2023-04-14 21:42:41 +0100

    update: added some custom message to errMsgString method

--- dist/src/utils/errMsgString.js
+++ dist/src/utils/errMsgString.js
@@ -9,6 +9,15 @@ function errMsgString(error) {
     else {
         errMsg = error === null || error === void 0 ? void 0 : error.message;
     }
+    if (errMsg === 'FAILURE_REASON_NO_ROUTE') {
+        errMsg = 'no route found';
+    }
+    if (errMsg === 'FAILURE_REASON_INSUFFICIENT_BALANCE') {
+        errMsg = 'insufficient balance';
+    }
+    if (errMsg === 'FAILURE_REASON_INCORRECT_PAYMENT_DETAILS') {
+        errMsg = 'incorrect payment details';
+    }
     return errMsg;
 }
 exports.errMsgString = errMsgString;
--- src/utils/errMsgString.ts
+++ src/utils/errMsgString.ts
@@ -5,5 +5,14 @@ export function errMsgString(error) {
   } else {
     errMsg = error?.message
   }
+  if (errMsg === 'FAILURE_REASON_NO_ROUTE') {
+    errMsg = 'no route found'
+  }
+  if (errMsg === 'FAILURE_REASON_INSUFFICIENT_BALANCE') {
+    errMsg = 'insufficient balance'
+  }
+  if (errMsg === 'FAILURE_REASON_INCORRECT_PAYMENT_DETAILS') {
+    errMsg = 'incorrect payment details'
+  }
   return errMsg
 }
