457cd64f647b7c8b05df4fa8a1038e800cc84d18
Oluwatobi Bamidele <teebams49@gmail.com>
2023-05-04 17:17:21 +0100

    feat: initialize test for delete message feature

---
+++ src/tests/controllers/deleteMessages.test.ts
@@ -0,0 +1,12 @@
+// import test from 'ava'
+// import nodes from '../nodes'
+// import { saveActionHistory } from '../utils/save'
+// import { verifyActionHistorySaved } from '../utils/get'
+
+// /*
+// npx ava src/tests/controllers/boostPayment.test.ts --verbose --serial --timeout=2m
+// */
+
+// test('test boostPayment: create tribe, join tribe, send messages, boost messages, leave tribe, delete tribe', async (t) => {
+//     await boostPayment(t, 0, 1, 2)
+//   })
