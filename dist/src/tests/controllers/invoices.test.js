e0673871b2cf34a11dc4d4548d6fe6951be7bce7
Oluwatobi Bamidele <teebams49@gmail.com>
2023-06-09 17:43:57 +0100

    update: added sleep for invoice test

--- dist/src/tests/controllers/invoices.test.js
+++ dist/src/tests/controllers/invoices.test.js
@@ -24,7 +24,7 @@ const helpers = require("../utils/helpers");
 function invoices(t, node1, node2) {
     return __awaiter(this, void 0, void 0, function* () {
         console.log(`Checking invoices for ${node1.alias} and ${node2.alias}`);
-        console.log(`${node1.alias} generating invoice to be paind by ${node2.alias}`);
+        console.log(`${node1.alias} generating invoice to be paid by ${node2.alias}`);
         //Create an Invoice
         const createdInvoice = yield (0, invoices_1.createInvoice)(t, node1, 12, 'test invoice');
         const paymentRequest = createdInvoice.response.invoice;
@@ -35,6 +35,7 @@ function invoices(t, node1, node2) {
         t.truthy(invoicePaymentRequest, 'Payment request should exist');
         //Payment request gotten from getInvoice should equal payment request from create invoice
         t.true(paymentRequest === invoicePaymentRequest, 'Payment request gotten from getInvoice should equal payment request from create invoice');
+        yield helpers.sleep(1000);
         //Node2 pays the invoice
         const paidInvoice = yield (0, invoices_1.payInvoice)(t, node2, paymentRequest);
         t.true(paidInvoice.success, 'Invoice should have been paid');
@@ -43,17 +44,25 @@ function invoices(t, node1, node2) {
         const invoiceDetail2 = yield (0, invoices_1.getInvoice)(t, node1, paymentRequest);
         const invoicePaymentStatus = invoiceDetail2.response.settled;
         t.true(invoicePaymentStatus, `Payment should have been made by ${node2.alias} to ${node1.alias}`);
-        console.log(`${node2.alias} generating invoice to be paind by ${node1.alias}`);
+        console.log(`${node2.alias} generating invoice to be paid by ${node1.alias}`);
         //Create an Invoice by node 2
+        yield helpers.sleep(1000);
         const createdInvoice2 = yield (0, invoices_1.createInvoice)(t, node2, 12, 'test invoice');
+        if (node1.alias === 'alice' && node2.alias === 'virtualNode0') {
+            console.log(createdInvoice2);
+        }
         const paymentRequest2 = createdInvoice2.response.invoice;
         t.truthy(paymentRequest2, 'Payment request should have been created');
         //Get Invoice details by node 2
         const invoiceDetail3 = yield (0, invoices_1.getInvoice)(t, node2, paymentRequest2);
         const invoicePaymentRequest2 = invoiceDetail3.response.payment_request;
-        t.truthy(invoicePaymentRequest2, 'Payment request should exist');
+        if (node1.alias === 'alice' && node2.alias === 'virtualNode0') {
+            console.log(invoiceDetail3);
+        }
+        t.truthy(invoicePaymentRequest2, `Payment request should exist for ${node2.alias} when testing with ${node1.alias}`);
         //Payment request gotten from getInvoice should equal payment request from create invoice
         t.true(paymentRequest2 === invoicePaymentRequest2, 'Payment request gotten from getInvoice should equal payment request from create invoice');
+        yield helpers.sleep(1000);
         //Node2 pays the invoice
         const paidInvoice2 = yield (0, invoices_1.payInvoice)(t, node1, paymentRequest2);
         t.true(paidInvoice2.success, 'Invoice should have been paid');
